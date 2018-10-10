708ec324a840d78c35e7b95966fdc2b8668595da
Andrew Brice <abrice@sharpencx.com>
2018-10-10 13:25:16 -0400

    env vars

--- routes/hero.js
+++ routes/hero.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var apiKeys = require('../apiKeys.js');
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
   let id = req.params.id;
-  request(`http://superheroapi.com/api/${apiKeys['SUPERHEROAPI_KEY']}/${id}`, function (error, response, body) {
+  request(`http://superheroapi.com/api/${process.env.SUPERHEROAPI_KEY}/${id}`, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       data = JSON.parse(body);
       res.json(data);
